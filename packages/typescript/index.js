42284f8783ee64ca5d8696b747857b15f986b362
Joshua Avalon <git@avalon.sh>
2022-04-05 15:08:34 +0800

    fix(typescript): fix prettier

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -3,8 +3,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint"
+    "plugin:prettier/recommended"
   ],
   rules: {
     "@typescript-eslint/array-type": "error",
