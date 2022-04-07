5f7c410e4e7f5276ca4af4ca37d8311097080a58
Joshua Avalon <git@avalon.sh>
2022-04-07 19:06:06 +0800

    fix(typescript): remove typecheck lint

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended"
   ],
   rules: {
