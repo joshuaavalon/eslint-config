2b2627941e0361bd3f42b434e5f17c240ed039b1
Joshua Avalon <git@avalon.sh>
2022-09-05 11:19:36 +0800

    fix: remove strict

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/strict",
     "plugin:prettier/recommended"
   ],
   plugins: ["@typescript-eslint"],
