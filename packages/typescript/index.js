5433f31efcba0dd97989294422746b94d01e0122
Joshua Avalon <git@avalon.sh>
2022-09-05 11:08:20 +0800

    fix(typescript): remove type check

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:@typescript-eslint/strict",
     "plugin:prettier/recommended"
   ],
