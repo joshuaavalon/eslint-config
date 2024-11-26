68068d61daebe4959d4546456135d034db6ed06a
Joshua Avalon <git@avalon.sh>
2024-11-26 09:56:52 +0800

    fix: Allow comment start

--- packages/typescript/src/stylistic.ts
+++ packages/typescript/src/stylistic.ts
@@ -1,8 +1,25 @@
 import type { PrefixRules } from "@joshuaavalon/eslint-config-javascript/utils";
 import type { UnprefixedRuleOptions } from "@stylistic/eslint-plugin";
 
+type TypeScriptRules = `@stylistic/type-${string}` & keyof PrefixRules<UnprefixedRuleOptions, "@stylistic/">;
+type CustomTypeScriptRules = "@stylistic/lines-around-comment";
 
-export const stylisticRules: Pick<PrefixRules<UnprefixedRuleOptions, "@stylistic/">, `@stylistic/type-${string}` & keyof PrefixRules<UnprefixedRuleOptions, "@stylistic/">> = {
+export const stylisticRules: Pick<PrefixRules<UnprefixedRuleOptions, "@stylistic/">, CustomTypeScriptRules | TypeScriptRules> = {
+  "@stylistic/lines-around-comment": [
+    "error",
+    {
+      afterHashbangComment: true,
+      allowArrayStart: true,
+      allowBlockStart: true,
+      allowClassStart: true,
+      allowEnumStart: true,
+      allowInterfaceStart: true,
+      allowModuleStart: true,
+      allowObjectStart: true,
+      allowTypeStart: true,
+      beforeBlockComment: true
+    }
+  ],
   "@stylistic/type-annotation-spacing": ["error"],
   "@stylistic/type-generic-spacing": ["error"],
   "@stylistic/type-named-tuple-spacing": ["error"]
